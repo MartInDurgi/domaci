5e8ce2ca621c5457a5888a741dcbd89a540dae13
MartInDurgi <martindurgala@gmail.com>
2023-08-30 19:50:47 +0200

    zadatak 2

--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -8,7 +8,7 @@ const [animals, setAnimals] = useState(() => {
 return ([
 { species: "dog", name: "rex", birthdate: JSON.stringify(new Date) },
 { species: "cat", name: "cica", birthdate: JSON.stringify(new Date) },
-{ species: "donkey", name: "lole", birthdate: JSON.stringify(new Date) },
+{ species: "donkey", name: "lole", },
 { species: "horse", name: "john", birthdate: JSON.stringify(new Date) },
 { species: "tiger", name: "max", birthdate: JSON.stringify(new Date) },
 ])
@@ -48,12 +48,11 @@ return (
             {
             animals.map((element, index) => {
             return (
-            <Animal key={index} 
-            remove={<button onClick={()=> removeAnimal(index)}>remove</button>}
+            <Animal key={index} remove={<button onClick={()=> removeAnimal(index)}>remove</button>}
                 top={<button onClick={()=> thisToTop(index)}>top</button>}
                 species={element.species}
                 name={element.name}
-                birthdate={element.birthdate.slice(1, 11)}
+                birthdate={element.birthdate ? element.birthdate.slice(1, 11) : "nepoznat"}
 
                 />
                 );
